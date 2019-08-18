f4bc4678297f12676fb6ccec39569afa0f71fc2b
satoutakeshi <sato.takeshi1987@gmail.com>
2019-08-18 14:39:13 +0900

    Initial Commit

---
+++ UpperTabUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpperTabUI
+//
+//  Created by satoutakeshi on 2019/08/18.
+//  Copyright © 2019 satoutakeshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
